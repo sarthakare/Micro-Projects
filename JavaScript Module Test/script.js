316f53580b2bcb95e348d3547efc614156b9c0e9
sarthak <thakresarthak6@gmail.com>
2023-12-20 19:36:23 +0530

    final submissions

--- JavaScript Module Test/script.js
+++ JavaScript Module Test/script.js
@@ -1,6 +1,8 @@
 // Initialize scores from localStorage or set them to 0
 let userScore = parseInt(localStorage.getItem('userScore')) || 0;
 let computerScore = parseInt(localStorage.getItem('computerScore')) || 0;
+let move = document.getElementById("rules-button");
+let win;
 
 // Function to update the score display
 function updateScoreDisplay() {
@@ -17,7 +19,7 @@ function saveScoresToLocalStorage() {
 // Function to determine the winner of a round
 function determineWinner(user, computer) {
     if (user === computer) {
-        return 'It\'s a tie!';
+        return 'TIE UP';
     } else if (
         (user === 'rock' && computer === 'scissors') ||
         (user === 'paper' && computer === 'rock') ||
@@ -26,12 +28,12 @@ function determineWinner(user, computer) {
         userScore++;
         updateScoreDisplay();
         saveScoresToLocalStorage();
-        return 'You win!';
+        return 'YOU WIN AGAINST PC';
     } else {
         computerScore++;
         updateScoreDisplay();
         saveScoresToLocalStorage();
-        return 'You lose!';
+        return 'YOU LOST AGAINST PC';
     }
 }
 
@@ -47,18 +49,22 @@ function userChoice(choice) {
 
     // Get computer's choice
     const computer = computerChoice();
+    //disable users choice buttons
+    document.getElementById('rock').style.display = 'none';
+    document.getElementById('paper').style.display = 'none';
+    document.getElementById('scissors').style.display = 'none';
 
+
+    document.getElementById('YoursChoice').style.display = 'block';
     if (choice === 'rock') {
-        document.getElementById('paper').style.display = 'none';
-        document.getElementById('scissors').style.display = 'none';
+        document.getElementById('uc-rock').style.display = 'block';
     }else if (choice === 'paper') {
-        document.getElementById('rock').style.display = 'none';
-        document.getElementById('scissors').style.display = 'none';
+        document.getElementById('uc-paper').style.display = 'block';
     }else{
-        document.getElementById('rock').style.display = 'none';
-        document.getElementById('paper').style.display = 'none';
+        document.getElementById('uc-scissors').style.display = 'block';
     }
 
+    document.getElementById('ComputerChoice').style.display = 'block';
     if (computer === 'rock') {
         document.getElementById('cc-rock').style.display = 'block';
     } else if (computer === 'paper') {
@@ -70,6 +76,11 @@ function userChoice(choice) {
     // Determine the winner and display the outcome
     const outcome = determineWinner(choice, computer);
     document.getElementById('outcome').innerText = outcome;
+    if (outcome === 'YOU WIN AGAINST PC') {
+        win = true;
+        document.getElementById('next-button').style.display = 'block';
+        move.className = move.className + " move";
+    }
 
     // Show the "Play Again" button
     document.getElementById('play-again').style.display = 'block';
@@ -82,17 +93,25 @@ function playAgain() {
     document.getElementById('rock').style.display = 'block';
     document.getElementById('paper').style.display = 'block';
     document.getElementById('scissors').style.display = 'block';
+    document.getElementById('uc-rock').style.display = 'none';
+    document.getElementById('uc-paper').style.display = 'none';
+    document.getElementById('uc-scissors').style.display = 'none';
     document.getElementById('cc-rock').style.display = 'none';
     document.getElementById('cc-paper').style.display = 'none';
     document.getElementById('cc-scissors').style.display = 'none';
+    document.getElementById('YoursChoice').style.display = 'none';
+    document.getElementById('ComputerChoice').style.display = 'none';
+    document.getElementById('next-button').style.display = 'none';
+    
+    if (win) {
+        move.className = move.className - " move";
+    }
 
     // Hide the "Play Again" button
     document.getElementById('play-again').style.display = 'none';
 
     // Clear the outcome text and user/computer choices
     document.getElementById('outcome').innerText = '';
-    document.getElementById('user-option').innerText = '';
-    document.getElementById('computer-option').innerText = '';
 }
 
 // Call updateScoreDisplay on page load to show scores
@@ -115,3 +134,25 @@ window.onclick = function (event) {
         modal.style.display = 'none';
     }
 };
+
+// Function to handle the "next" button click
+function next() {
+    // Hide the game container
+    document.getElementById('game-container').style.display = 'none';
+
+    // Show the winning image
+    document.getElementById('winning-img').style.display = 'block';
+    document.getElementById('play-game-again').style.display = 'block';
+    document.getElementById('confetti').style.display = 'block';
+    document.getElementById('next-button').style.display = 'none';
+    move.className = move.className - " move";
+}
+
+function playGameAgain() {
+    document.getElementById('winning-img').style.display = 'none';
+    document.getElementById('play-game-again').style.display = 'none';
+    document.getElementById('game-container').style.display = 'block';
+    document.getElementById('confetti').style.display = 'none';
+    playAgain();
+}
+
